5062486ca06fff01ca91a0163da7fa3ea0b68030
diegoarmiliato <degoarmiliato@gmail.com>
2023-11-10 17:52:17 -0300

    Adicionando LOGs de execução

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # ----------------------------- VARIÁVEIS ----------------------------- #
+LOG_FILE="./log/install-$(date +%T).log"
+mkdir -p log
+exec > >(tee ${LOG_FILE}) 2>&1
+
+
 URL_REAL_VNC="https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-7.6.1-Linux-x64.rpm"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
@@ -13,15 +18,19 @@ PROGRAMAS_PARA_INSTALAR=(
   gh
   zsh
 )
+
+COLOR='\033[1;36m'         # Cyan
+NC='\033[0m'               # No Color
+
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- REQUISITOS ----------------------------- #
 ## Atualizando o repositório ##
-echo "#### $(date +%T) - ATUALIZANDO REPOSITÓRIOS"
+echo -e "${COLOR}#### $(date +%T) - ATUALIZANDO REPOSITÓRIOS${NC}"
 sudo dnf update -y
 
 ## Alterando configurações DNF
-echo "#### $(date +%T) - ADICIONANDO PARAMETROS DNF.CONF"
+echo -e "${COLOR}#### $(date +%T) - ADICIONANDO PARAMETROS DNF.CONF${NC}"
 cat /etc/dnf/dnf.conf
 if ! cat /etc/dnf/dnf.conf | grep -q "max_parallel_downloads"; then echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf; fi
 if ! cat /etc/dnf/dnf.conf | grep -q "fastestmirror"; then echo "fastestmirror=true" | sudo tee -a /etc/dnf/dnf.conf; fi
@@ -29,21 +38,21 @@ if ! cat /etc/dnf/dnf.conf | grep -q "deltarpm"; then echo "deltarpm=true" | sud
 cat /etc/dnf/dnf.conf
 
 ## Atualizando o repositório ##
-echo "#### $(date +%T) - ATUALIZANDO REPOSITÓRIOS"
+echo -e "${COLOR}#### $(date +%T) - ATUALIZANDO REPOSITÓRIOS${NC}"
 sudo dnf update -y
 
-echo "#### $(date +%T) - HABILITAR RPM FUSION FREE"
+echo -e "${COLOR}#### $(date +%T) - HABILITAR RPM FUSION FREE${NC}"
 ## Habilitar RPM Fusion Free
 sudo dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 
-echo "#### $(date +%T) - HABILITAR RPM FUSION NONFREE"
+echo -e "${COLOR}#### $(date +%T) - HABILITAR RPM FUSION NONFREE${NC}"
 ## Habilitar RPM Fusion Nonfree
 sudo dnf install \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 # Repositório Visual Studio Code
-echo "#### $(date +%T) - HABILITAR REPOSITÓRIO VISUAL STUDIO CODE"
+echo -e "${COLOR}#### $(date +%T) - HABILITAR REPOSITÓRIO VISUAL STUDIO CODE${NC}"
 cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
 [code]
 name=Visual Studio Code
@@ -53,11 +62,11 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 
-echo "#### $(date +%T) - EXECUTANDO DNF UPGRADE"
+echo -e "${COLOR}#### $(date +%T) - EXECUTANDO DNF UPGRADE${NC}"
 ## Atualizar com os dados dos repositórios adicionados
 sudo dnf upgrade --refresh
 
-echo "#### $(date +%T) - INSTALAR CODECS DE MIDIA"
+echo -e "${COLOR}#### $(date +%T) - INSTALAR CODECS DE MIDIA${NC}"
 ## Instalar CODECs de Mídia
 sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
 sudo dnf install lame\* --exclude=lame-devel -y
@@ -67,10 +76,10 @@ sudo dnf group upgrade --with-optional Multimedia -y --allowerasing
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
 ## Atualizando o repositório depois da adição de novos repositórios ##
-echo "#### $(date +%T) - ATUALIZANDO REPOSITÓRIOS"
+echo -e "${COLOR}#### $(date +%T) - ATUALIZANDO REPOSITÓRIOS${NC}"
 sudo dnf update -y
 
-echo "#### $(date +%T) - INSTALANDO PACOTES RPM"
+echo -e "${COLOR}#### $(date +%T) - INSTALANDO PACOTES RPM${NC}"
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_REAL_VNC"       -P "$DIRETORIO_DOWNLOADS"
@@ -79,25 +88,25 @@ wget -c "$URL_REAL_VNC"       -P "$DIRETORIO_DOWNLOADS"
 sudo rpm -i $DIRETORIO_DOWNLOADS/*.rpm
 rm -rf "$DIRETORIO_DOWNLOADS"
 
-echo "#### $(date +%T) - INSTALANDO APPS VIA DNF"
+echo -e "${COLOR}#### $(date +%T) - INSTALANDO APPS VIA DNF${NC}"
 # Instalar programas no dnf
 for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
     sudo dnf install "$nome_do_programa" -y
 done
 
 ## Instalando pacotes Flatpak ##
-echo "#### $(date +%T) - INSTALANDO APPS VIA FLATPAK"
+echo -e "${COLOR}#### $(date +%T) - INSTALANDO APPS VIA FLATPAK${NC}"
 flatpak install flathub com.mattjakeman.ExtensionManager -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak install flathub nz.mega.MEGAsync -y
 flatpak install flathub com.rtosta.zapzap -y
 
-echo "#### $(date +%T) - CONFIGURAÇÕES GLOBAIS GIT"
+echo -e "${COLOR}#### $(date +%T) - CONFIGURAÇÕES GLOBAIS GIT${NC}"
 git config --global user.email "degoarmiliato@gmail.com"
 git config --global user.name "diegoarmiliato"
 
 ## Instalando Extensões Gnome ##
-echo "#### $(date +%T) - INSTALANDO EXTENSÕES GNOME"
+echo -e "${COLOR}#### $(date +%T) - INSTALANDO EXTENSÕES GNOME${NC}"
 git clone https://github.com/ToasterUwU/install-gnome-extensions.git
 cd install-gnome-extensions
 chmod +x install-gnome-extensions.sh
@@ -106,17 +115,17 @@ cd ..
 rm -rf ./install-gnome-extensions
 
 ## Instalando Oh-My-Zsh
-echo "#### $(date +%T) - INSTALANDO OH-MY-ZSH"
+echo -e "${COLOR}#### $(date +%T) - INSTALANDO OH-MY-ZSH${NC}"
 CURRENT_DIR=$(pwd)
 cd ~
-if [ -d ".oh-my-zsh" ]; then
+if [ ! -d ".oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  echo "#### $(date +%T) - INSTALANDO PLUGIN AUTOSUGGESTION ZSH"
+  echo -e "${COLOR}#### $(date +%T) - INSTALANDO PLUGIN AUTOSUGGESTION ZSH${NC}"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   sed -i 's/plugins=(git/plugins=(git zsh-autosuggestions docker docker-compose gh/g' ~/.zshrc
 
-  echo "#### $(date +%T) - INSTALANDO TEMA DRACULA ZSH"
+  echo -e "${COLOR}#### $(date +%T) - INSTALANDO TEMA DRACULA ZSH${NC}"
   cd Downloads
   git clone https://github.com/dracula/zsh.git
   cd zsh
