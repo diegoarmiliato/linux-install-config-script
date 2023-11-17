55058ddebd4ee15a94c818608277b3e7b69c4e87
diegoarmiliato <degoarmiliato@gmail.com>
2023-11-17 17:19:27 -0300

    Melhorando script de instalação NVIDIA

--- nvidia.sh
+++ nvidia.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
+LOG_FILE="./log/nvidia-$(date +%T).log"
+mkdir -p log
+exec > >(tee ${LOG_FILE}) 2>&1
 
-echo "#### $(date +%T) - INSTALANDO FERRAMENTAS NECESSÁRIAS"
-sudo dnf install kmodtool akmods mokutil openssl -y
+COLOR='\033[1;36m'         # Cyan
+NC='\033[0m'               # No Color
 
-echo "#### $(date +%T) - GERANDO CHAVE"
-sudo kmodgenca -a
+if sudo bash -c '[ ! -f "/etc/pki/akmods/certs/public_key.der" ]'; then 
+    echo -e "${COLOR}#### $(date +%T) - INSTALANDO FERRAMENTAS NECESSÁRIAS${NC}"
+    sudo dnf install kmodtool akmods mokutil openssl -y
 
-echo "#### $(date +%T) - CONFIGURANDO A CHAVE"
-sudo mokutil --import /etc/pki/akmods/certs/public_key.der
+    echo -e "${COLOR}#### $(date +%T) - GERANDO CHAVE${NC}"
+    sudo kmodgenca -a
 
-echo "#### $(date +%T) - REINICIANDO EM 10 SEGUNDOS"
-sleep 10s
-sudo reboot
+    echo -e "${COLOR}#### $(date +%T) - CONFIGURANDO A CHAVE${NC}"
+    sudo mokutil --import /etc/pki/akmods/certs/public_key.der
 
-echo "#### $(date +%T) - INSTALANDO DRIVERS NVIDIA"
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 -y --allowerasing
+    echo -e "${COLOR}#### $(date +%T) - REINICIANDO EM 10 SEGUNDOS${NC}"
+    sleep 10s
+    sudo reboot
+else
+    echo -e "${COLOR}#### $(date +%T) - INSTALANDO DRIVERS NVIDIA${NC}"
+    sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 -y --allowerasing
 
-echo "#### $(date +%T) - AGUARDANDO 10 MINUTOS PARA QUE SEJAM CARREGADOS OS MÓDULOS"
-sleep 10m
-echo "#### $(date +%T) - ESPERA FINALIZADA"
+    echo -e "${COLOR}#### $(date +%T) - AGUARDANDO 10 MINUTOS PARA QUE SEJAM CARREGADOS OS MÓDULOS${NC}"
+    sleep 10m
 
-echo "#### $(date +%T) - ATUALIZANDO MÓDULOS KERNEL"
-sudo akmods --force
-sudo dracut --force
+    echo -e "${COLOR}#### $(date +%T) - ESPERA FINALIZADA${NC}"
 
-echo "#### $(date +%T) - REINICIANDO EM 10 SEGUNDOS"
-sleep 10s
-sudo reboot
+    echo -e "${COLOR}#### $(date +%T) - ATUALIZANDO MÓDULOS KERNEL${NC}"
+    sudo akmods --force
+    sudo dracut --force
+
+    echo -e "${COLOR}#### $(date +%T) - REINICIANDO EM 10 SEGUNDOS${NC}"
+    sleep 10s
+    sudo reboot
+fi    
